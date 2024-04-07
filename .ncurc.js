981eac453218c2bdabfe63fdf35504ecc74a9b69
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-07 11:47:27 -0700

    Update deps

--- .ncurc.js
+++ .ncurc.js
@@ -3,6 +3,7 @@
 module.exports = {
     target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
         if (dependencyName === "@types/node") return "minor";
+        if (dependencyName === "eslint") return "minor";
         if (major === "0") return "minor";
         return "latest";
     },

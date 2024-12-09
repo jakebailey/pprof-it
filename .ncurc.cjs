f1523ec1120a5181ddef15d62a2cb38aea606fab
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-12-08 22:02:00 -0800

    Update eslint

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,9 +1,8 @@
 // @ts-check
-/** @type {import('npm-check-updates').RunOptions}*/
 module.exports = {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
         if (dependencyName === "@types/node") return "minor";
-        if (dependencyName === "eslint") return "minor";
         if (major === "0") return "minor";
         return "latest";
     },
